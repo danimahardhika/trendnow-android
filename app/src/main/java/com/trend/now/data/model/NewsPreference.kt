59a475992102b638cce6b372d97d498943b94412
Dani Mahardhika <danimahardhika112@gmail.com>
2025-03-05 20:06:48 +0700

    chore: improve local news section (#13)
    * update news cache
    
    * update db version
    
    * improve app route
    
    * improve local news section

--- app/src/main/java/com/trend/now/data/model/NewsPreference.kt
+++ app/src/main/java/com/trend/now/data/model/NewsPreference.kt
@@ -3,4 +3,10 @@ package com.trend.now.data.model
 data class NewsPreference(
     val language: String = "",
     val country: String = ""
-)
+) {
+
+    /**
+     * @return Whether the [language] and [country] is not blank or blank
+     */
+    fun isNotBlank(): Boolean = language.isNotBlank() && country.isNotBlank()
+}
